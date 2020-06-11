e8deba4811a648dd96910dfb3f5b54707ad3455b
ZioCecio <gabricetro@libero.it>
2020-06-11 19:35:18 +0200

    Aggiunta prova Dijkstra piu rapido

--- PgAr2020_Shadowing_LeRovinePerdute/src/Main.java
+++ PgAr2020_Shadowing_LeRovinePerdute/src/Main.java
@@ -5,9 +5,13 @@ import utils.XmlManager;
 
 public class Main {
     public static void main(String[] args) {
-       //System.out.println(XmlManager.readCities("./input/PgAr_Map_5.xml"));
-    	Country theCountry= new Country(XmlManager.readCities("./input/PgAr_Map_2000.xml"));
-    	System.out.println("parsing avvenuto");
-    	System.out.println(new PathManagerAle(theCountry).getBestPath());
+        // System.out.println(XmlManager.readCities("./input/PgAr_Map_5.xml"));
+        Country theCountry = new Country(
+                XmlManager.readCities("./PgAr2020_Shadowing_LeRovinePerdute/input/PgAr_Map_10000.xml"));
+        System.out.println("parsing avvenuto");
+        long startTime = System.nanoTime();
+        System.out.println(new PathManagerAle(theCountry).getBestPath());
+        long stopTime = System.nanoTime();
+        System.out.println(stopTime - startTime);
     }
 }
