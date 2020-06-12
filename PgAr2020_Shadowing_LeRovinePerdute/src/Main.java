f35162f5b470b23b4d9195a106cd79e9cc0255a1
ale-art <a.frosi001@studenti.unibs.it>
2020-06-12 07:23:51 +0200

    controlli su 10000.xml

--- PgAr2020_Shadowing_LeRovinePerdute/src/Main.java
+++ PgAr2020_Shadowing_LeRovinePerdute/src/Main.java
@@ -2,7 +2,6 @@ import java.util.Collection;
 
 import city.City;
 import city.Country;
-import path.distance.PathManagerAle;
 import path.height.PathManagerHeight;
 import utils.XmlManager;
 
@@ -11,7 +10,7 @@ public class Main {
     public static void main(String[] args) {
         // System.out.println(XmlManager.readCities("./input/PgAr_Map_5.xml"));
         Country theCountry = new Country(
-                XmlManager.readCities("./PgAr2020_Shadowing_LeRovinePerdute/input/PgAr_Map_50.xml"));
+                XmlManager.readCities("./input/PgAr_Map_10000.xml"));
         System.out.println("parsing avvenuto");
         long startTime = System.nanoTime();
         // System.out.println(new PathManagerAle(theCountry).getBestPath());
